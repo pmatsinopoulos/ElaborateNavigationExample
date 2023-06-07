dab06d8d4801e07d2824505016a9ec3506bb0b76
Panos Matsinopoulos <panos@mixlr.com>
2023-06-07 18:32:18 +0300

    Clicking on Terms navigates to terms fragment

--- app/src/main/java/com/mixlr/panos/elaboratenavigationexample/HomeFragment.kt
+++ app/src/main/java/com/mixlr/panos/elaboratenavigationexample/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.mixlr.panos.elaboratenavigationexample.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -19,6 +20,9 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.btnTerms.setOnClickListener {
+            it.findNavController().navigate(R.id.action_frgmntHome_to_frgmntTerms)
+        }
         return binding.root
     }
 }
